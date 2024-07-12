ee2cd991603ab56e19db0f8a459b92d80cfaa096
luanAfons0 <luan.hmatao@gmail.com>
2024-07-12 15:50:15 -0300

    variables

---
+++ variables.rb
@@ -0,0 +1,38 @@
+# Variáveis
+# Variável é uma referência de um valor que é armazenado na memória do sistema.
+# Em Ruby qualquer palavra simples em minúsculas, é uma variável.
+# E elas podem ser compostas por letras, dígitos e travessões. Por exemplo:
+x = 0
+y = 1
+cachorro133 = "cachorro133"
+cachorro_caramelo = "cachorro caramelo"
+
+# Constantes
+# Constantes são variáveis que não podem ser alteradas. Por exemplo:
+Nome = "Ruby"
+# Nome = "PHP" # variables.rb:11: warning: previous definition of Nome was here
+
+# Variáveis Locais
+# Variáveis locais são variáveis que só existem dentro de um bloco. Por exemplo:
+x = 10
+
+# Variáveis Globais
+# Variáveis que começam com um cifrão são globais. Ou seja, elas são acessíveis de qualquer lugar do código.
+$x
+$y
+$cachorro133
+$cachorro_caramelo
+
+# Variáveis de Instância
+# Variáveis que começam com uma arroba são variáveis de instância. Ou seja, elas são acessíveis apenas dentro de um método.
+@x
+@y
+@cachorro133
+@cachorro_caramelo
+
+# Variáveis de Classe
+# Variáveis que começam com duas arrobas são variáveis de classe. Ou seja, elas são acessíveis apenas dentro de uma classe.
+@@x
+@@y
+@@cachorro133
+@@cachorro_caramelo
