2b642a6c88699449d29cbc9b31477b580bfefa28
luanAfons0 <luan.hmatao@gmail.com>
2024-07-12 15:52:56 -0300

    Arithmetic Operators

---
+++ arithmetic-operators.rb
@@ -0,0 +1,37 @@
+# Operadores Aritméticos
+# Os operadores aritméticos padrões são:
+
+# Soma	+	Soma dois valores
+# Subtração	-	Subtrai dois valores
+# Multiplicação	*	Multiplica dois valores
+# Divisão	/	Divide dois valores
+# Módulo	%	Resto de divisão
+# Potência	**	Calcula a potência entre dois números
+
+10 + 5 # 15
+
+10 - 5 # 5
+
+10 * 5 # 50
+
+10 / 5 # 2
+
+10 % 5 # 0
+
+10 ** 5 # 100000
+
+# 10 / 0 # ZeroDivisionError: divided by 0
+
+# Operadores Aritméticos com Variáveis
+# No capítulo sobre as variáveis mostramos como declarar variáveis e você também pode usar esses operadores com as variáveis ensinadas anteriormente. Como por exemplo:
+
+x = 10
+y = 20
+
+x + y # 30
+x - y # -10
+x * y # 200
+x / y # 0
+x % y # 10
+x ** y # 1000000
+
